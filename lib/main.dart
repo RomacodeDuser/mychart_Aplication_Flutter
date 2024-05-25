c49d1d354e76018b41d334d808365d63f768a590
Roma.code <romadani.code@gmail.com>
2024-05-26 00:17:36 +0700

    02

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:kopichart/injectable.dart';
 import 'package:kopichart/presentation/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  configureDependencies();
   runApp(const App());
 }
