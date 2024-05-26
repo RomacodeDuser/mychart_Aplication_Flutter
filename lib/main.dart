4735e3c05adcb9f6929da66b1f4a1e0a3db06ed4
Roma.code <romadani.code@gmail.com>
2024-05-26 08:01:55 +0700

    03

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kopichart/injectable.dart';
 import 'package:kopichart/presentation/app.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   configureDependencies();
   runApp(const App());
 }
